3856a9a175d07e124703578ac6b1bfc684a3c68a
iptime-fox <dudndi826@gmail.com>
2023-07-06 21:20:36 +0900

    #6.18 Enums

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#fff',
+  accentColor: '#FF9EAA',
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: '#F2F4F6',
+  textColor: '#2f3640',
+  accentColor: '#FF9EAA',
+};
